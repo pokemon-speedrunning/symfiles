c4c90c13c4f02a0c7150ee569bacb78f1563df0c
entrpntr <entrpntr@gmail.com>
2020-03-08 14:05:18 -0400

    More script cleanup.

--- scripts/savwram.rb
+++ scripts/savwram.rb
@@ -49,38 +49,38 @@ class SavWram
     end
   end
 
-  def make()
+  def make_txt
     # hardcode some stuff for the main logic to work
     case @repo
     when "pokered", "pokeyellow"
+      add_rby_hardcodes
       w_start_prefix = "Start"
-      s_sections = ["PlayerName", "MainData", "SpriteData", "PartyData", "BoxData", "TilesetType"]
-      rby_ws_hardcodes()
+      s_section_names = ["PlayerName", "MainData", "SpriteData", "PartyData", "BoxData", "TilesetType"]
     when "pokegold", "pokecrystal"
+      add_gsc_hardcodes
       w_start_prefix = ""
-      s_sections = ["Options", "GameData", "CrystalData"]
-      gsc_ws_hardcodes()
+      s_section_names = ["Options", "GameData", "CrystalData"]
     else
-      raise ArgumentError.new(format("Invalid repo (%s). Expected one of poke[red|yellow|gold|crystal].", @repo))
+      raise ArgumentError.new("Invalid repo (#{@repo}). Expected one of poke[red|yellow|gold|crystal].")
     end
 
     # common code to write the "wram/sav address" mappings for rby+gsc
-    s_sections.each do |section|
-      w_start_label = "w" + section + w_start_prefix
+    s_section_names.each do |name|
+      w_start_label = "w" + name + w_start_prefix
       next unless @wram[w_start_label]
 
-      w_end_label = "w" + section + "End"
-      s_base = @sram["s" + section]
+      w_end_label = "w" + name + "End"
+      s_base_addr = @sram["s" + name]
 
       (@wram[w_start_label]...@wram[w_end_label]).each do |w_addr|
         w_label = get_label(w_addr)
-        s_addr = s_base + w_addr - @wram[w_start_label]
-        print(format("\n%s = %04X (%04X)", w_label, w_addr, s_addr))
+        s_addr = s_base_addr + w_addr - @wram[w_start_label]
+        puts format("%s = %04X (%04X)", w_label, w_addr, s_addr)
       end
     end
   end
 
-  def rby_ws_hardcodes()
+  def add_rby_hardcodes
     # wram/sram hardcodes
     @wram["wPlayerNameStart"] = @wram["wPlayerName"]
     @wram["wPlayerNameEnd"] = @wram["wPlayerNameStart"] + 11
@@ -89,7 +89,7 @@ class SavWram
     @sram["sBoxData"] = @sram["sCurBoxData"]
   end
 
-  def gsc_ws_hardcodes()
+  def add_gsc_hardcodes
     # wram/sram hardcodes
     @sram["sOptions"] = sav_addr(0x01, 0xA000)
     @sram["sGameData"] = sav_addr(0x01, 0xA009)
@@ -98,15 +98,16 @@ class SavWram
   def get_label(addr)
     # get the lowest-level label for the address
     floor = addr.downto(0) { |n| break n if @wram.has_key?(n) }
-    "#{format("%04X", addr - floor)}+#{@wram[floor]}"
+    format("%04X+%s", addr - floor, @wram[floor])
   end
 end
 
 out_name = repo + ".txt"
-puts "Outputting to " + out_name
+puts "Outputting to #{out_name}"
 
 File.open(File.join(__dir__, out_name), "w") do |out_file|
   $stdout = out_file
-  puts "wram label = wram addr (sav addr)\n"
-  SavWram.new(repo).make()
+  puts "wram label = wram addr (sav addr)"
+  puts
+  SavWram.new(repo).make_txt
 end
